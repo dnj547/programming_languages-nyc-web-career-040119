3f23f370a737ab7d139d5108abdc8d45415f5488
Danielle Jasper <dnj547@live.com>
2019-03-13 23:02:43 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,9 +3,8 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_hash|
     language_hash.each do |language, type_hash|
-      binding.pry
       new_hash[language] = type_hash
-      new_hash[:style] = style
+      new_hash[language][:style] = style
     end
   end
   new_hash
