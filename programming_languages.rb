43eac88767b5cfe63b69645843b4cf1ab523e1bd
Danielle Jasper <dnj547@live.com>
2019-03-13 23:24:04 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,10 +3,14 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, language_hash|
     language_hash.each do |language, type_hash|
-      new_hash[language] = type_hash
-      style_array = []
-      style_array << style
-      new_hash[language][:style] = style_array
+      if new_hash.has_key?(language)
+        new_hash[language][:style] << style
+      else
+        new_hash[language] = type_hash
+        style_array = []
+        style_array << style
+        new_hash[language][:style] = style_array
+      end
     end
   end
   new_hash
